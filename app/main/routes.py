206ea36a65e49e4f9be22a41dc41e0de1fd0b7fb
LuisCastilloH <andres.castillo.hdez@gmail.com>
2018-05-06 11:55:41 -0500

    Minor changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -23,10 +23,10 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    else:
-        global parallelFlag
         global userLogged
-        parallelFlag = 0
+        userLogged = 1
+    else:
+        global userLogged
         userLogged = 0
     g.locale = str(get_locale())
 
